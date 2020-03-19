44a7acbc1a9bbcbf80984e663ca5178f45759ce8
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-19 23:30:52 +0900

    add cors settings

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema';
 import resolvers from './resolver';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, cors: true });
 
 server.listen().then(({ url }) => {
   console.log(`GraphQL-Pokemon started on ${url}`);
